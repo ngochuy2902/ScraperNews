f2cf282974d4d71de088857d1e3166a6d11adea6
Huy Ho Ngoc <huyhn@nal.vn>
2021-03-22 16:21:42 +0700

    Add launcher run all spider

--- news/news/launcher.py
+++ news/news/launcher.py
@@ -16,5 +16,22 @@ def run_spider(spider_name: str, **kwargs):
     process.start()
 
 
+def run_all_spider():
+    project_settings = get_project_settings()
+    spider_loader = SpiderLoader(project_settings)
+    spider_names = spider_loader.list()
+    spider_names.remove(BaseSpider.name)
+    # Init and run process crawler
+    process = CrawlerProcess(settings=project_settings)
+    for name in spider_names:
+        spider_cls = spider_loader.load(spider_name=name)
+        if not issubclass(spider_cls, BaseSpider):
+            raise ValueError(f"Spider {spider_cls.name} must inherit from class {BaseSpider.name}")
+        process.crawl(crawler_or_spidercls=spider_cls)
+    process.start(stop_after_crawl=True)
+    process.join()
+
+
 if __name__ == "__main__":
-    run_spider(spider_name="thanhnien")
+    # run_spider(spider_name="dantri")
+    run_all_spider()
